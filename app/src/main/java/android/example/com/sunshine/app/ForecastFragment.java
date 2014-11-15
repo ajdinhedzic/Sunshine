cc4c9d2a7244d3d1b2e5029ad2d84dca5a8c3528
ajdinhedzic <ahedzic@gmail.com>
2014-11-15 15:25:20 -0600

    refactor - moved forecastfragment to own class

---
+++ app/src/main/java/android/example/com/sunshine/app/ForecastFragment.java
@@ -0,0 +1,100 @@
+package android.example.com.sunshine.app;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Ajdin on 11/15/2014.
+ */
+public class ForecastFragment extends Fragment {
+
+    public ForecastFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        List<String> weekForecast = new ArrayList<String>();
+        weekForecast.add("Today - Sunny - 88/63");
+        weekForecast.add("Tomorrow - Foggy - 88/63");
+        weekForecast.add("Weds - Cloudy - 88/63");
+        weekForecast.add("Thurs - Rainy - 88/63");
+        weekForecast.add("Fri - Foggy - 88/63");
+        weekForecast.add("Sat - Sunny - 88/63");
+
+        ArrayAdapter<String> mForecastAdapter = new ArrayAdapter<String>(getActivity(),
+                R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview,
+                weekForecast);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(mForecastAdapter);
+
+
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+
+        String forecastJsonStr = null;
+        try {
+            URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
+
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if (inputStream == null) {
+                forecastJsonStr = null;
+            }
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            //appending  to JSON makes debugging easier
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line + "\n");
+            }
+
+            if (buffer.length() == 0) {
+                forecastJsonStr = null;
+            }
+            forecastJsonStr = buffer.toString();
+        } catch (IOException e) {
+            Log.e("ForecastFragment", "Error ", e);
+            forecastJsonStr = null;
+        } finally{
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (final IOException e) {
+                    Log.e("ForecastFragment", "Error closing stream", e);
+                }
+            }
+        }
+
+
+        return rootView;
+    }
+
+
+
+
+}
